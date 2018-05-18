5abee8865961a68950551b3bdaa6a433412a513c
Troels Krøgh <troels.kroegh@gmail.com>
2018-05-18 17:51:45 +0200

    Standardized item model

--- origin-generator/origin.py
+++ origin-generator/origin.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from zmq import Context
 
 FREE_ITEMS_URL = r"https://data1.origin.com/template/store/free-games/on-the-house.en-us.dnk.directive"
-STORE_BASE = r"https://www.origin.com/dnk/en-us/store"
+STORE_BASE_URL = r"https://www.origin.com/dnk/en-us/store"
 ALL_URL = r"https://api4.origin.com/supercat/DK/en_DK/supercat-PCWIN_MAC-DK-en_DK.json.gz"
 TIMEOUT = 60
 
@@ -34,11 +34,16 @@ def get_free_items():
     for item in on_the_house_items:
         game_path = item["ocd-path"]
         game_info = [b for b in all_offers['offers'] if b["offerPath"] == item['ocd-path']][0]
-        print(f"dbg: {game_info['itemName']}: {STORE_BASE + game_path}")
-        yield game_info
+        print(f"dbg: {game_info['itemName']}: {STORE_BASE_URL + game_path}")
+        item = {
+            "name": game_info["itemName"],
+            "url": STORE_BASE_URL + game_path,
+            "price": "0.0",
+        }
+        yield item
 
 while True:
-    topic = b"1"
+    topic = b"0"
     free_stuff = list(get_free_items())
 
     message = json.dumps(free_stuff).encode("utf-8")
